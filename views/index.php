c06e79b930bb7ed5fadf8c6985d82ed51e8fa309
artoo <artoo@inventati.org>
2019-05-07 11:52:53 +0100

    --

--- views/index.php
+++ views/index.php
@@ -1,19 +1,21 @@
 <?php include('_header.php');?>
 
 
-<div style="text-align:left" class="boxcenter">
+<div style="text-align:left; " class="boxcenter">
 	<div class="menu">
 		Open Phoda-SeV
 	</div>
 </div>
 
 <div style="background:#FFFFFF; text-align: center;">
-	<br><br><br>
+	
 	<div style="padding-left: 5%;float: left;">
 		<form action="exp.php">	
+			<br><br><br>
 			<button class="button" type="submit" value="Begin Test">Access Experiments</button>
 		</form>
 	</div>
+
 	<div class="sections">
 		<?php echo LANDING_PAGE; ?>
 	</div>
