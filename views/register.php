6c6a551b8dec3973de8c89991002deb8d790519a
artoo-git <scriem@gmail.com>
2025-07-15 17:58:06 +0200

    created more space betwe pw and PI naming,  and clarified the fields

--- views/register.php
+++ views/register.php
@@ -8,7 +8,7 @@
 	    <tr><td><input id="exp_name" type="text" pattern="[a-zA-Z0-9]{2,64}" name="exp_name" placeholder="experiment name" required /></td></tr>
 	    
 	    <tr><td><input id="password" type="password" name="password" placeholder="experiment password" pattern=".{6,}" required autocomplete="off" /></td></tr>
-	    
+	    <tr><td>Principal Investigator</td></tr>
 	    <tr><td><input id="pi" type="text" name="pi" placeholder="principal investigator" required autocomplete="off" /></td></tr>
 	    
     	<tr><td><button class="button" type="submit" name="register" value="register">register</button></td></tr>
